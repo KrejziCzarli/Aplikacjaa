b7a94bdc4eae3b8efa00ed1b75a47f2febcb8105
charl <charlesss999@gmail.com>
2023-03-15 17:30:23 +0100

    a

--- Czelendżapp/Czelendżapp/Program.cs
+++ Czelendżapp/Czelendżapp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// else if
+
+
+var name = "Karol";
+var age = 65;
+
+
+if (age > 10)
+{
+    if (age > 65)
+    {
+        Console.WriteLine("Mam więcej niż 50 lat");
+    }
+    else if (name == "Kaarol")
+    {
+        Console.WriteLine("Mam więcej niż 50 lat i mam na imię Karol");
+    }
+}
+
+else if (age > 10) ;
+{
+    if (age > 70)
+    {
+        Console.WriteLine("ka");
+    }
+    else
+    {
+        Console.WriteLine("ba");
+    }
+
+}
+
